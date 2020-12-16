d5ead371de4272996a8c55e48aa52d55e331ebf0
Eduardo Ruiz <eduruiz333@gmail.com>
2020-12-16 12:17:34 -0300

    Conceitos introdutorios

--- assets/scripts/imprimir-html.js
+++ assets/scripts/imprimir-html.js
@@ -4,7 +4,7 @@ new Vue({
 
     data: {
         linkHTML: '<a href="https://www.google.com" target="_blank">Imprimindo link em HTML</a>'
-    },
+    }
 })
 
 // Uma vez que estamos trabalhando dentro de um contexto de uma propriedade, nós vamos usar diretamente o nome dela sem o double mustache
