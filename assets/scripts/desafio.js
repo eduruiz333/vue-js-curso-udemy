28a7acb961381e25e917643485905e6ecad265d8
Eduardo Ruiz <eduruiz333@gmail.com>
2020-12-17 19:58:05 -0300

    Resoluçao dos desafios

--- assets/scripts/desafio.js
+++ assets/scripts/desafio.js
@@ -3,11 +3,16 @@ new Vue ({
     el: '#desafio',
 
     data: {
+        // Dados Pessoais
         nome: 'Edu Nagual',
         idade: 48,
 
+        // Números para randomizar
         min: 1,
-        max: 10
+        max: 10,
+
+        // Imagem para inserir via bind
+        image: 'https://cdn.wegow.com/media/artists/dead-can-dance/dead-can-dance-1492555236.47.2560x1440.jpg'
     },
 
     methods: {
