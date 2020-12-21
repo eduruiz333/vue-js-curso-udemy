83a7e53c2d10dc0a93d0a359611986cfbc261500
Eduardo Ruiz <eduruiz333@gmail.com>
2020-12-21 19:18:32 -0300

    Eventos

--- assets/scripts/05-eventos.js
+++ assets/scripts/05-eventos.js
@@ -2,13 +2,30 @@ new Vue({
     el: '#app',
 
     data: {
-        contador: 0
+        contador: 0,
+        contador5: 5,
+
+        x: 0,
+        y: 0,
+
+        cinco: 5
     },
 
     methods: {
+
         somar() {
             this.contador++
             console.log(this.contador)
+        },
+
+        atualizarXY(event) {
+            this.x = event.clientX
+            this.y = event.clientY
+        },
+
+        somar5(ev) {
+            this.contador5 += this.cinco
+            console.log(ev)
         }
     }
 })
