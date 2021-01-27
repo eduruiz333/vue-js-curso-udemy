457a8e075ffb8415ad48920499585d45a638c582
Eduardo Ruiz <eduruiz333@gmail.com>
2021-01-27 09:55:30 -0300

    Condicionais

---
+++ assets/scripts/01-condicionais.js
@@ -0,0 +1,11 @@
+// NESTE ARQUIVO ENCONTRAM-SE OS EXERCÍCIOS 1, 2 E 3 DE CONDICIONAIS
+
+new Vue({
+    el: '#app',
+
+    data: {
+        nome: 'Maria',
+        logado: false, 
+        anonimo: false
+    }
+})
