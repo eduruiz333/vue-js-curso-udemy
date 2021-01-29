cdcffb5066b46a2e7d1ba67f8578c923d73cc4a6
Eduardo Ruiz <eduruiz333@gmail.com>
2021-01-29 09:17:41 -0300

    exercicio de listas

---
+++ assets/scripts/02-listas.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+
+    data: {
+        cores: ['azul', 'amarelo', 'verde', 'vermelho', 'preto'],
+
+        pessoas: [
+            {
+                nome: 'Rose',
+                idade: 28,
+                peso: 60
+            },
+            {
+                nome: 'José',
+                idade: 39,
+                peso: 58
+            }
+        ]
+    }
+})
