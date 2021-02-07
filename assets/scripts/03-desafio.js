6585d30b0cfad73d06dffb6a70e76383736bd114
Eduardo Ruiz <eduruiz333@gmail.com>
2021-02-07 12:16:23 -0300

    Desafio de listas e condicionais

---
+++ assets/scripts/03-desafio.js
@@ -0,0 +1,24 @@
+new Vue ({
+    el: '#desafio',
+
+    data: {
+
+        verP1: false,
+        verP2: false,
+        expressao: true,
+
+        artista: ['Dead Can Dance', 'Suns of Arqa', 'Pink Floyd', 'Enya', 'Loreena'],
+
+        livros: {
+            titulo: 'O Senhor dos Anéis',
+            autor: 'J. R. R, Tokiens',
+            volume: '3'
+        },
+
+        aluno: {
+            id: '10',
+            nome: 'Eduardo Ruiz',
+            notas: [7.65, 8.33, 6.98, 9.21]
+        }
+    }
+})
