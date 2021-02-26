30c55170b71da3d6e4bed0d783c0dbc15a532761
Eduardo Ruiz <eduruiz333@gmail.com>
2021-02-26 09:36:39 -0300

    Criação e importação de componente

---
+++ 04-componetes/proj-components/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div id="app">
+      <h1> Contador </h1>
+      <app-contador />
+      <app-contador />
+      <app-contador />
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ 04-componetes/proj-components/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import Contador from './Contador.vue'
+
+Vue.config.productionTip = false
+Vue.component('app-contador', Contador)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
