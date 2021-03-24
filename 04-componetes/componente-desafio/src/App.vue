11ed9c6d83048a7ca6bb58975585928ec80348e8
Eduardo Ruiz <eduruiz333@gmail.com>
2021-03-24 12:18:46 -0300

    Desafio componentização

---
+++ 04-componetes/componente-desafio/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <app-menu />
+
+    <v-app-bar app>
+      <app-toolbar />
+    </v-app-bar>
+
+    <v-content>
+      <app-carousel />
+    </v-content>
+
+    <app-footer />
+
+  </v-app>
+</template>
+
+<script>
+  import Menu from './Menu'
+  import Toolbar from './Toolbar'
+  import Carousel from './Carousel'
+  import Footer from './Footer'
+
+  export default {
+    components: {
+      'app-menu': Menu,
+      'app-toolbar': Toolbar,
+      'app-carousel': Carousel,
+      'app-footer': Footer
+    }
+  }
+</script>
---
+++ 04-componetes/proj-components/src/Contadores.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="contadores">
+        <app-contador />
+    </div>
+</template>
+
+<script>
+import Contador from './Contador'
+export default {
+    component: { 'app-contador': Contador }
+}
+</script>
+
+<style>
+    
+</style>
